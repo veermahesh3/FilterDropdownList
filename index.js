ba3e1e519607122a13145931918750d69961aed8
veermahesh3 <48675547+veermahesh3@users.noreply.github.com>
2023-06-23 02:42:15 -0700

    Add files via upload
    searching or filtering the name from list of dropdown.

---
+++ index.js
@@ -0,0 +1,21 @@
+function myFunction(){
+  document.getElementById('myDropdown').classList.toggle("show");
+}
+
+
+function filterFunction(){
+let input=document.getElementById("myInput");
+ let filter=input.value.toUpperCase();
+
+ let div=document.getElementById("myDropdown");
+ let a=div.getElementsByTagName('a');
+
+ for(let i=0;i<a.length;i++){
+let txtValue=a[i].innerText || a[i].textContent;
+  if(txtValue.toUpperCase().indexOf(filter)>-1){
+    a[i].style.display="";
+  }else{
+    a[i].style.display="none";
+  }
+ }
+}
