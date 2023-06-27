3f373b12b0ab1b85a321057c99b6b106a447db8d
veermahesh3 <veermahesh3@gmail.com>
2023-06-27 03:02:28 -0700

    cc

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 
- //
- //
- //
+function myFunction(){
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+function filterFunction(){
+let input=document.getElementById("myInput");
+let filter=input.value.toUppercase();
+let div=document.getElementById("myDropdown");
+let a=div.getElementsByTagName("a");
+for(let i=0;i<a.length;i++){
+txtValue=a[i].innerText || a[i].textContent;
+if(txtValue.toUppercase().indexOf(filter)>-1){
+  a[i].style.display="";
+}
+else{
+  a[i].style.display="none";
+}
+
+}
+
+
+
+}
