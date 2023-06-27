8ffdd0fdcad0e96a6276fc88b6b6fd6f9c486cc6
veermahesh3 <veermahesh3@gmail.com>
2023-06-27 03:06:14 -0700

    done

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ function myFunction(){
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-
 function filterFunction(){
 let input=document.getElementById("myInput");
 let filter=input.value.toUppercase();
@@ -11,15 +10,11 @@ let div=document.getElementById("myDropdown");
 let a=div.getElementsByTagName("a");
 for(let i=0;i<a.length;i++){
 txtValue=a[i].innerText || a[i].textContent;
-if(txtValue.toUppercase().indexOf(filter)>-1){
+if(txtValue.toUppercase().indexOf(filter) > -1){
   a[i].style.display="";
 }
 else{
   a[i].style.display="none";
 }
-
 }
-
-
-
 }
